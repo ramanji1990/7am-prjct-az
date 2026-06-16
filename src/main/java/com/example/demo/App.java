fb6f3155c8622406a99a8828e93edbca6f70182a
ramanji1990 <130961086+ramanji1990@users.noreply.github.com>
2026-06-16 08:21:51 +0530

    Update App.java

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -1,12 +1,22 @@
 package com.example.demo;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-
     public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        System.out.println("Hello from Java AKS App");
+        server.createContext("/", exchange -> {
+            String response = "Hello from Java AKS App";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
 
-        // Keep container running
-        Thread.sleep(Long.MAX_VALUE);
+        server.start();
+        System.out.println("Server started on port 8080");
     }
 }
