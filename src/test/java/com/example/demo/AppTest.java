18707141f59d30b7de2d1b97200ffe38c1273e33
RamanjiReddyvempalli <ramanji.devops@gmail.com>
2026-06-16 07:31:00 +0530

    code

---
+++ src/test/java/com/example/demo/AppTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
