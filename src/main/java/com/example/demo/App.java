3601e99a15c9b9f6ecb655d91286f8f532a8a77c
ramanji1990 <130961086+ramanji1990@users.noreply.github.com>
2026-06-16 08:14:47 +0530

    Update App.java

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
 public class App {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
+
         System.out.println("Hello from Java AKS App");
+
+        // Keep container running
+        Thread.sleep(Long.MAX_VALUE);
     }
 }
